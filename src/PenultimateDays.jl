85021d9ab7c4e908766092de2149500b0f84442f
Jake Ireland <jakewilliami@icloud.com>
2022-06-26 16:47:41 +1200

    Extract dates stdlib extended stubs

--- src/PenultimateDays.jl
+++ src/PenultimateDays.jl
@@ -7,11 +7,18 @@ for t in (:week, :month, :quarter, :year)
     @eval $pf(dt::TimeType) = error("not yet implemented")
     @eval $pf(dt::TimeType, d::Int) = error("not yet implemented")
     @eval export $pf
+end
+
+for t in (:week, :month, :quarter, :year)
     for m in (:first, :last)
         ef = Symbol("$(m)dayof$(t)")
-        @eval import Dates: $ef
-        @eval $ef(dt::TimeType, d::Int) = error("not yet implemented")
-        @eval export $ef
+        @eval begin
+            import Dates: $ef
+            function $ef(dt::TimeType, d::Int)
+                error("not yet implemented")
+            end
+            export $ef
+        end
     end
 end
 
