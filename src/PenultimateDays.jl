63d7929712da593366b44c83c041549bd874f594
Jake Ireland <jakewilliami@icloud.com>
2022-06-26 18:31:31 +1200

    Implement exended function lastdayofmonth

--- src/PenultimateDays.jl
+++ src/PenultimateDays.jl
@@ -13,24 +13,34 @@ end
 
 for m in (:first, :last), t in (:week, :month, :quarter, :year)
     ef = Symbol("$(m)dayof$(t)")
-    @eval begin
-        import Dates: $ef
-        export $ef
-    end
+    @eval import Dates: $ef
 end
 
+## Week
 function firstdayofweek(dt::TimeType, d::Int)
     fd_date = firstdayofweek(dt)
     return fd_date + Day(d - 1)
 end
+
 lastdayofweek(dt::TimeType, d::Int) = firstdayofweek(dt, d)
 
+## Month
 function firstdayofmonth(dt::TimeType, d::Int)
     fd_date = firstdayofmonth(dt)
     fd_i = dayofweek(fd_date)
-    fd_day = d - fd_i + 1
-    fd_i > d && (fd_day += 7)
+    fd_day = d - fd_i + 1 + 7(fd_i > d)
     return fd_date + Day(fd_day - 1)
 end
+
+function lastdayofmonth(dt::TimeType, d::Int)
+    ld_date = lastdayofmonth(dt)
+    ld_i = dayofweek(ld_date)
+    ld_day = 7(ld_i < d) - d + ld_i + 1
+    return ld_date - Day(ld_day - 1)
+end
+
+## Quarter
+
+## Year
 
 end  # end module
