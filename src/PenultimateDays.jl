c7469d5c9b10fa504f0eb0a79a597588e4b5cbcd
Jake Ireland <jakewilliami@icloud.com>
2022-06-26 17:11:09 +1200

    Implement extended function firstdayofmonth

--- src/PenultimateDays.jl
+++ src/PenultimateDays.jl
@@ -9,17 +9,22 @@ for t in (:week, :month, :quarter, :year)
     @eval export $pf
 end
 
-for t in (:week, :month, :quarter, :year)
-    for m in (:first, :last)
-        ef = Symbol("$(m)dayof$(t)")
-        @eval begin
-            import Dates: $ef
-            function $ef(dt::TimeType, d::Int)
-                error("not yet implemented")
-            end
-            export $ef
-        end
+# Dates (stdlib) extended
+
+for m in (:first, :last), t in (:week, :month, :quarter, :year)
+    ef = Symbol("$(m)dayof$(t)")
+    @eval begin
+        import Dates: $ef
+        export $ef
     end
+end
+
+function firstdayofmonth(dt::TimeType, d::Int)
+    fd_date = firstdayofmonth(dt)
+    fd_i = dayofweek(fd_date)
+    fd_day = d - fd_i + 1
+    fd_i > d && (fd_day += 7)
+    return fd_date + Day(fd_day - 1)
 end
 
 end  # end module
